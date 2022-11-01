c165eb1102088a702f25cbdec5c98eb59ec63a58
Mr Adi <107009032+Mradi007@users.noreply.github.com>
2022-11-01 11:31:59 +0500

    Update Dump33.py

--- Dump33.py
+++ Dump33.py
@@ -1,13 +1,25 @@
 import os, platform
+
 try:
+
     import requests
+
 except:
+
     os.system('pip install requests')
+
 import requests
+
 bit = platform.architecture()[0]
+
 if bit == '64bit':
-    from adi64 import main
+
+    from dump64 import main
+
     main()
+
 elif bit == '32bit':
-    from adi32 import main
+
+    from dump32 import main
+
     main()
